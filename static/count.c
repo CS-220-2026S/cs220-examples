0c1c11c6d36c38294f65665cc4193a597d307943
tkilcoy1 <tkilcoy1@cs.binghamton.edu>
2026-02-09 11:52:31 -0500

    added/updated example

---
+++ static/count.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void counter();
+
+int main(){
+  // TODO: call the counter function
+  return 0;
+}
+
+void counter(){
+  // TODO: write a function that keeps count of the number of times it was called
+  
+}
--- vector/vect.c
+++ vector/vect.c
@@ -4,22 +4,31 @@
 struct vector{
   int *start;
   int len;
-}; // Question: how many bytes is this struct?
+}; // Question: how many bytes is this struct? 16 bytes.
 
 struct vector *createVector(int len){
-  return NULL;
+  struct vector *newVect = malloc(sizeof(struct vector));
+  newVect->start = malloc(len*sizeof(int));
+  newVect->len = len;
+  return newVect;
 }
 
 void printVector(struct vector *vect){
-  
+  for (int i = 0; i < vect->len; i++){
+    printf("index %d = %d\n",i,vect->start[i]);
+  }
 }
 
 void freeVector(struct vector *vect){
-  
+  free(vect->start);
+  free(vect);
 }
 
 int main(int argc, char *argv[]){
   struct vector *vect = createVector(argc-1);
+  for (int i = 1; i < argc; i++){
+    vect->start[i-1] = atoi(argv[i]);
+  }
   printVector(vect);
   freeVector(vect);
   return 0;

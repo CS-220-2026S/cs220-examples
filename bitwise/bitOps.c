8c09c5e33082d5d9ea9a0a9d75d71cb7e59a865a
tkilcoy1 <tkilcoy1@cs.binghamton.edu>
2026-02-15 13:51:54 -0500

    updated examples

--- bitwise/bitOps.c
+++ bitwise/bitOps.c
@@ -2,17 +2,20 @@
 #include <stdlib.h>
 
 void setBit(unsigned long *val, int pos){
-  // TODO: set the bit in pos 'pos' to 1
+  // set the bit in pos 'pos' to 1
+  *val |= (1UL << pos);
 }
-
 void flipBit(unsigned long *val, int pos){
-  // TODO: toggle the bit in position 'pos' from 0 to 1 or from 1 to 0
+  // toggle the bit in position 'pos' from 0 to 1 or from 1 to 0
+  *val ^= (1UL << pos);
 }
-
 void clearBit(unsigned long *val, int pos){
-  // TODO: set the bit in position 'pos' to 0
+  // set the bit in position 'pos' to 0
+  *val &= ~(1UL << pos);
 }
-
 void flipBitFld(unsigned long *val, int len, int pos){
   // TODO: toggle the 'len' bits starting in position 'pos'
+  unsigned long mask = (1UL << len) - 1;
+  mask <<= pos;
+  *val ^= mask;
 }
--- matrix/matr.c
+++ matrix/matr.c
@@ -6,7 +6,14 @@
 
 int main(){
   int matr[ROW][COL] = {{1,2,3},{5,6,7},{9,10,11},{13,14,15}};
-  // TODO: print out the address of each entry in matr
-  
+  // TODO: print the address of each entry in matr
+  for (int i = 0; i < ROW; i++){
+    for (int j = 0; j < COL; j++){
+      printf("&matr[%d][%d] = %p\n", i, j, &matr[i][j]);
+    }
+  }
+  printf("matr[0][5] = %d\n", matr[0][5]);
+  printf("matr[1][4] = %d\n", matr[1][4]);
+  printf("matr[4][-5] = %d\n", matr[4][-5]);
   return 0;
 }

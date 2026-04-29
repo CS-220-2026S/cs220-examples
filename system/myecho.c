55063c3065d59a69e7d3a69ea85669dd36ad8951
tkilcoy1 <tkilcoy1@cs.binghamton.edu>
2026-04-29 14:43:15 -0400

    updated examples

--- system/myecho.c
+++ system/myecho.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
+#include <string.h>
 
 #define SIZE 64
 
 int main(){
   // TODO: in an loop, read a 64-byte message from stdin
   // print the message back
+  char msg[SIZE];
+  while(1){
+    printf("> ");
+    fgets(msg,sizeof(msg),stdin);
+    printf("%s",msg);
+    if (!strchr(msg, '\n')){
+      while(getchar() != '\n');
+      putchar('\n');
+    }
+    
+  }
   return 0;
 }
--- system/mysystem.c
+++ system/mysystem.c
@@ -4,14 +4,27 @@
 #include <signal.h>
 #include <sys/wait.h>
 
+void handler(int sig){
+  printf("\nTerminating process...\n");
+}
+
 int mysystem(char *cmd){
   // TODO: run the command 'cmd' in a shell
   // return the exit status of the process
-  return 0;
+  signal(SIGINT,handler);
+  int status;
+  if (fork() == 0){
+    signal(SIGINT,SIG_DFL);
+    char *argv[] = {"/bin/sh", "-c", cmd, NULL};
+    execve("/bin/sh",argv,NULL);
+  }
+  wait(&status);
+  return status;
 }
 
 int main(){
   mysystem("./myecho");
+  mysystem("pwd");
   mysystem("ls -l");
   return 0;
 }
